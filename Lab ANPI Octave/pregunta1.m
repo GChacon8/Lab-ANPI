7198aa8bf80d5906ce3154adf5a275801cbf4762
GChacon8 <gabriel.chacon.alfaro@gmail.com>
2023-11-07 14:55:58 -0600

    Pregunta en Octave

---
+++ Lab ANPI Octave/pregunta1.m
@@ -0,0 +1,23 @@
+function X=pregunta1()
+  tiempos1 = [];
+  tiempos2 = [];
+  for i=5:12
+    A=rand(2^i, 2^(i-1));
+    tic
+    [U,S,V] = svd(A);
+    tiempos1 = [tiempos1, toc];
+    tic
+    [Ur,Sr,Vr] = svdCompact(A);
+    tiempos2 = [tiempos2, toc];
+  endfor
+
+  x = [5,6,7,8,9,10,11,12];
+  plot(x, tiempos1, 'LineWidth', 2, 'Color', 'red', 'DisplayName', 'SVD Octave');
+  hold on;
+  plot(x, tiempos2, 'LineWidth', 2 , 'Color', 'blue', 'DisplayName', 'SVD Compact');
+  title("Gráfico SVD vs SVD-Compact");
+  xlabel("Parametro k");
+  ylabel("Tiempo");
+  legend;
+   grid on;
+  endfunction
